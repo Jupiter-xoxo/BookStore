bab071591f07e5b104379b2ab40753f46dd6bfc9
HeroBoy <teeratadpi@gmail.com>
2018-09-27 21:36:02 +0700

    init project

---
+++ workspace/BookStoreWebApp/src/main/java/com/co/th/bookstore/BookStoreWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.co.th.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreWebAppApplication.class, args);
+	}
+}
